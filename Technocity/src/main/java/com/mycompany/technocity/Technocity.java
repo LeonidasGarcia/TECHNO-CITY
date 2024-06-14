1b094f6082fc695376500e0c896cc2ed3043d9f7
Leo <leonidasgarcialescano@gmail.com>
2024-06-13 21:54:12 -0500

    cambiando a español

---
+++ Technocity/src/main/java/com/mycompany/technocity/Technocity.java
@@ -0,0 +1,12 @@
+package com.mycompany.technocity;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Technocity {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
